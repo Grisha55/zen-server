f72e6151ed535f57e8d741750166d50e23f573c9
Grisha55 <grishavinyar@icloud.com>
2025-07-26 13:52:33 +0300

    Refactored router.js

--- src/middleware/router.js
+++ src/middleware/router.js
@@ -1,29 +1,29 @@
-import { routes } from '../routes/apiRoutes.js';
-import { handlePostRequest } from '../controllers/dataController.js';
-import { preflightRequestHandler, setCorsHeaders } from './cors.js';
+import { routes } from "../routes/apiRoutes.js";
+import { handlePostRequest } from "../controllers/dataController.js";
+import { preflightRequestHandler, setCorsHeaders } from "./cors.js";
+
 
 export const serverRequestHandler = (req, res) => {
-  const endpoint = req.url ?? '';
-  console.log({ endpoint, method: req.method });
+  const endpoint = req.url ?? "";
 
   setCorsHeaders(res);
-  
-  if (req.method === 'OPTIONS') {
+
+  if (req.method === "OPTIONS") {
     return preflightRequestHandler(res);
   }
 
-  if (req.method === 'POST' && endpoint === '/save-data') {
+  if (req.method === 'POST' && req.url === '/save-data') {
     return handlePostRequest(req, res);
   }
 
-  const route = routes.find(r => 
-    endpoint === r.endpoint && req.method === r.method
+  const route = routes.find(
+    (r) => endpoint === r.endpoint && req.method === r.method
   );
-  
+
   if (route) {
     return route.handler(req, res);
   }
 
-  res.writeHead(404, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ error: 'Route not found' }));
+  res.writeHead(404, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ error: "Route not found" }));
 };
